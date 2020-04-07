224124b05d3a46091f322bb1ef5f06a65c3b852c
Luis Flosi <54824168+LuisFlosi@users.noreply.github.com>
2020-04-07 18:04:32 -0500

    Update on poisson model

--- Trips_Poisson_Predictions.R
+++ Trips_Poisson_Predictions.R
@@ -3,38 +3,56 @@ lapply(pkgs, require, character.only = T)
 cores<-detectCores()
 cl <- makeCluster(cores[1]-1)
 registerDoParallel(cl)
-set.seed(02252020)
+set.seed(04072020)
 
-arriving = read.csv("~/arriving_structuredaaa.csv",sep = "/")
+arriving = read.csv("~/MScA/3. Optimization and Simulation/Final Project/arriving_structured.csv")
 arriving["station_id"] = lapply(arriving["station_id"], as.factor)
 arriving["end_hour"] = lapply(arriving["end_hour"], as.factor)
 arriving["end_month"] = lapply(arriving["end_month"], as.factor)
 arr_lim_cols = arriving[c("station_id", "end_hour", "end_month", "arriving_trips", "tempC", "visibility", "day_of_week")]
 
-leaving = read.csv(paste(dataPath,"leaving_structuredlll.csv",sep = "/"))
+leaving = read.csv("~/MScA/3. Optimization and Simulation/Final Project/leaving_structured.csv")
 
 leaving["station_id"] = lapply(leaving["station_id"],as.factor)
 leaving["start_hour"] = lapply(leaving["start_hour"],as.factor)
 leaving["start_month"] = lapply(leaving["start_month"],as.factor)
 leav_lim_cols = leaving[c("station_id", "start_hour", "start_month", "leaving_trips", "tempC", "visibility", "day_of_week")]
 
-sz = 0.05*nrow(leav_lim_cols)
+sz = round(0.005*nrow(leav_lim_cols))
 
 train_leav = leav_lim_cols[sample(nrow(leav_lim_cols), sz), ]
 tab <- table(train_leav$station_id)
-train_leav = train_leav[train_leav$station_id %in% names(tab)[tab>50],]
+train_leav = train_leav[train_leav$station_id %in% names(tab)[tab>10],]
 
 train_arr = arr_lim_cols[sample(nrow(arr_lim_cols), sz), ]
-train_arr = train_arr[train_arr$station_id %in% names(tab)[tab>50],]
+train_arr = train_arr[train_arr$station_id %in% names(tab)[tab>10],]
 
-train_leav = leav_lim_cols
+weekdays = function(dta){
+  for (i in dta) {  
+    if (i %in% c("Monay","Tuesday","Wednesday","Thursday","Friday")){
+      return("Weekday")
+    } else {
+      return("Weekend")
+    }
+  }
+}
 
-train_arr = arr_lim_cols
+train_arr["days_of_week"] = sapply(train_arr$day_of_week, weekdays)
+train_arr["days_of_week"] = sapply(train_arr$days_of_week, as.factor)
+train_arr["day_of_week"] = NULL
+train_leav["days_of_week"] = sapply(train_leav$day_of_week, weekdays)
+train_leav["days_of_week"] = sapply(train_leav$days_of_week, as.factor)
+train_leav["day_of_week"] = NULL
 
-arr_pred = glm(arriving_trips ~ ., data = train_arr, family = "poisson")
+arr_pred = glm(arriving_trips ~ station_id*end_hour + tempC + days_of_week, data = train_arr, family = "poisson")
+summary(arr_pred)
+arr_pred$deviance / arr_pred$df.residual # no over dispersion
+pchisq(arr_pred$deviance, arr_pred$df.residual, lower.tail=F) # shows good fit
 
-leav_pred = glm(leaving_trips ~ ., data = train_leav, family = "poisson")
-
+leav_pred = glm(leaving_trips ~ (station_id + start_hour)^2 + tempC + days_of_week, data = train_leav, family = "poisson")
+summary(leav_pred)
+leav_pred$deviance / leav_pred$df.residual # no over dispersion
+pchisq(leav_pred$deviance, leav_pred$df.residual, lower.tail=F) # shows good fit
 
 test = expand.grid(as.factor(unlist(unique(train_arr["station_id"]))),
                    as.factor(c(0:23)),
@@ -43,22 +61,15 @@ test = expand.grid(as.factor(unlist(unique(train_arr["station_id"]))),
                    c(10),
                    c("Weekday"))
 
-colnames(test) = c("station_id","end_hour","end_month","tempC","visibility","day_of_week")
+colnames(test) = c("station_id","end_hour","end_month","tempC","visibility","days_of_week")
 
 arrive_preds = predict(arr_pred, newdata = test, type = "response")
+
+colnames(test) = c("station_id","start_hour","start_month","tempC","visibility","days_of_week")
 
 test_arr = test
 
 test_arr["predicted_arr"] = arrive_preds
-
-test = expand.grid(as.factor(unlist(unique(train_leav["station_id"]))),
-                   as.factor(c(0:23)),
-                   as.factor(c(7)),
-                   c(25),
-                   c(10),
-                   c("Weekday"))
-
-colnames(test) = c("station_id","start_hour","start_month","tempC","visibility","day_of_week")
 
 # Bootstraping function from https://www.r-bloggers.com/prediction-intervals-for-poisson-regression/
 boot_pi <- function(model, pdata, n, p) {
@@ -87,12 +98,11 @@ test_leav["predicted_leav_upper"] = leave_preds[3]
 colnames(test_arr) = c("station_id","start_hour","start_month","tempC","visibility","day_of_week", "predicted_arr")
 
 station_hourly_preds = merge(test_arr, test_leav, on = c("station_id", "start_hour", "start_month", "tempC", "visibility", "day_of_week"))
-station_hourly_preds[7:9] = lapply(station_hourly_preds[7:9], round)
+station_hourly_preds[7:9] = lapply(station_hourly_preds[7,9,10], round)
 
 # Save results
 
 res <- list(leave_preds=leave_preds,
             arrive_preds=arrive_preds,
-            station_hourly_preds=station_hourly_preds,
-            boot_pi = boot_pi)
-saveRDS(res, file = paste(dataPath,'result.rds',sep = '/'))    
+            station_hourly_preds=station_hourly_preds)
+saveRDS(res, file = '~/MScA/3. Optimization and Simulation/Final Project/test_preds.rds')    
