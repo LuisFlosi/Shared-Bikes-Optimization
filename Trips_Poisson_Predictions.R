6d19a48c721f3ad219be979d8345b04f12e24942
Luis Flosi <54824168+LuisFlosi@users.noreply.github.com>
2020-04-09 12:17:31 -0500

    Updated poisson models

--- Trips_Poisson_Predictions.R
+++ Trips_Poisson_Predictions.R
@@ -3,6 +3,8 @@ lapply(pkgs, require, character.only = T)
 cores<-detectCores()
 cl <- makeCluster(cores[1]-1)
 registerDoParallel(cl)
+
+# Data prep
 set.seed(04072020)
 
 arriving = read.csv("~/MScA/3. Optimization and Simulation/Final Project/arriving_structured.csv")
@@ -27,6 +29,12 @@ train_leav = train_leav[train_leav$station_id %in% names(tab)[tab>10],]
 train_arr = arr_lim_cols[sample(nrow(arr_lim_cols), sz), ]
 train_arr = train_arr[train_arr$station_id %in% names(tab)[tab>10],]
 
+set.seed(07042020)
+test_leav = leav_lim_cols[sample(nrow(leav_lim_cols), sz), ]
+test_leav = test_leav[test_leav$station_id %in% names(tab)[tab>10],]
+test_arr = arr_lim_cols[sample(nrow(arr_lim_cols), sz), ]
+test_arr = test_arr[test_arr$station_id %in% names(tab)[tab>10],]
+
 weekdays = function(dta){
   for (i in dta) {  
     if (i %in% c("Monay","Tuesday","Wednesday","Thursday","Friday")){
@@ -37,23 +45,70 @@ weekdays = function(dta){
   }
 }
 
+hour_group = function(i){
+    if (i %in% c(0,1,2,3,4,5,6,7)){
+      return(as.factor("0"))
+    } else if (i %in% c(8,9,10)) {
+      return(as.factor("8"))
+    } else if (i %in% c(11,12,13,14,15,16)) {
+      return(as.factor("11"))
+    } else if (i %in% c(17,18,19)) {
+      return(as.factor("17"))
+    } else {
+      return(as.factor("20"))
+    }
+}
+
+
 train_arr["days_of_week"] = sapply(train_arr$day_of_week, weekdays)
 train_arr["days_of_week"] = sapply(train_arr$days_of_week, as.factor)
 train_arr["day_of_week"] = NULL
+train_arr["hour_group"] = sapply(as.numeric(train_arr$end_hour),hour_group)
 train_leav["days_of_week"] = sapply(train_leav$day_of_week, weekdays)
 train_leav["days_of_week"] = sapply(train_leav$days_of_week, as.factor)
 train_leav["day_of_week"] = NULL
+train_leav["hour_group"] = sapply(as.numeric(train_leav$start_hour),hour_group)
 
-arr_pred = glm(arriving_trips ~ station_id*end_hour + tempC + days_of_week, data = train_arr, family = "poisson")
+test_arr["days_of_week"] = sapply(test_arr$day_of_week, weekdays)
+test_arr["days_of_week"] = sapply(test_arr$days_of_week, as.factor)
+test_arr["day_of_week"] = NULL
+test_arr["hour_group"] = sapply(as.numeric(test_arr$end_hour),hour_group)
+
+test_leav["days_of_week"] = sapply(test_leav$day_of_week, weekdays)
+test_leav["days_of_week"] = sapply(test_leav$days_of_week, as.factor)
+test_leav["day_of_week"] = NULL
+test_leav["hour_group"] = sapply(as.numeric(test_leav$start_hour),hour_group)
+
+
+
+# Arrivals model
+arr_pred = glm(arriving_trips ~  station_id*hour_group + hour_group + tempC + station_id*days_of_week, data = train_arr, family = "poisson")
 summary(arr_pred)
 arr_pred$deviance / arr_pred$df.residual # no over dispersion
-pchisq(arr_pred$deviance, arr_pred$df.residual, lower.tail=F) # shows good fit
+pchisq(arr_pred$deviance, arr_pred$df.residual, lower.tail=F) # shows bad fit
 
-leav_pred = glm(leaving_trips ~ (station_id + start_hour)^2 + tempC + days_of_week, data = train_leav, family = "poisson")
+test_arr["predicted_arr"] = ceiling(predict(arr_pred, newdata = test_arr))
+test_arr["arr_error"] = test_arr$arriving_trips - test_arr$predicted_arr
+arr_errs = test_arr$arr_error
+hist(arr_errs[arr_errs < 20], main ="distribution of arrival errors", xlab = "error") # right-tailed errors, with -1 to 5 as the range
+
+
+# Departures (Leaving) predictions
+leav_pred = glm(leaving_trips ~ station_id*hour_group + hour_group + tempC + station_id*days_of_week, data = train_leav, family = "poisson")
 summary(leav_pred)
 leav_pred$deviance / leav_pred$df.residual # no over dispersion
-pchisq(leav_pred$deviance, leav_pred$df.residual, lower.tail=F) # shows good fit
+pchisq(leav_pred$deviance, leav_pred$df.residual, lower.tail=F) # shows bad fit
 
+test_leav["predicted_leav"] = ceiling(predict(leav_pred, newdata = test_leav))
+test_leav["leav_error"] = test_leav$leaving_trips - test_leav$predicted_leav
+leav_errs = test_leav$leav_error
+hist(leav_errs[leav_errs < 20], main ="distribution of departure errors", xlab = "error") # right-tailed errors, with -1 to 5 as the range
+
+par(mfrow=c(1,2))
+hist(arr_errs[arr_errs < 20], main ="distribution of arrival errors", xlab = "error") # right-tailed errors, with -1 to 5 as the range
+hist(leav_errs[leav_errs < 20], main ="distribution of departure errors", xlab = "error") # right-tailed errors, with -1 to 5 as the range
+
+# Predict "standard case"
 test = expand.grid(as.factor(unlist(unique(train_arr["station_id"]))),
                    as.factor(c(0:23)),
                    as.factor(c(7)),
